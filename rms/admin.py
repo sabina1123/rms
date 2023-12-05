cd836761e3602710730f60b9acded97032ce4dd3
bogatisabina <bogatisabina41@gmail.com>
2023-12-05 15:17:59 +0545

    models.py

--- rms/admin.py
+++ rms/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(OrderItems)
+class OrderItemAdmin(admin.ModelAdmin):
+    pass
+
+    
--- rms/models.py
+++ rms/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User=get_user_model()
 
 # Create your models here.
 class Category(models.Model):
@@ -17,3 +19,43 @@ class Food(models.Model):
 
     def __str__(self):
         return self.name
+    
+    
+class Table(models.Model):
+
+    number=models.IntegerField()
+    is_occupied=models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Table no {self.number}"
+    
+class Order(models.Model):
+    COMPLETED_CHOICE='C'
+    PENDING_CHOICE='P'
+    
+    ORDER_STATUS_CHOICES=[
+        (COMPLETED_CHOICE,'COMPLETED'),
+        (PENDING_CHOICE,'PENDING'),
+    ]
+
+    
+    table=models.ForeignKey(Table,on_delete=models.CASCADE)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    total_price=models.IntegerField()
+    status=models.CharField(max_length=1,choices=ORDER_STATUS_CHOICES,default='PENDING_CHOICE')
+    payment_status=models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class OrderItems(models.Model):
+
+    food=models.ForeignKey(Food,on_delete=models.PROTECT)
+    order=models.ForeignKey(Order,on_delete=models.PROTECT)
+  
+
+
+
